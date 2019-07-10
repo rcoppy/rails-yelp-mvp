fa9daa3788978e38e3b22f238ef0684d2eaa5a8a
Alexander Rupp-Coppi <amr2311@columbia.edu>
2019-07-10 18:00:03 +0200

    links

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,14 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
-    Review.create(review_params)
-    Review.restaurant_id = params[:id]
-    redirect_to restaurant_path(params[:id])
+    review = Review.create(review_params)
+    review.restaurant_id = params[:restaurant_id]
+    review.save
+    redirect_to restaurant_path(params[:restaurant_id])
   end
 
   private
